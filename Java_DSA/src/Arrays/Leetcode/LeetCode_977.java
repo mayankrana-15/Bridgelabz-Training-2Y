bbd985a4393315636cbc84b4a04bbf8e040e563e
Mayank Rana <mr9536893@gmail.com>
2026-08-19 08:14:15 +0530

    [Mayank Rana]Java question submitted on 19 August

---
+++ Java_DSA/src/Arrays/Leetcode/LeetCode_977.java
@@ -0,0 +1,18 @@
+package Arrays.Leetcode;
+import java.util.*;
+public class LeetCode_977 {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int nums[] = new int[size];
+        
+    }
+}
